9fa40b30801991724c135654c53dbf807ad02869
Maike Simoncini da Silva <maikesimoncinims@gmail.com>
2024-11-05 23:52:49 -0300

    Update script3.js

--- script3.js
+++ script3.js
@@ -9,13 +9,13 @@ botao.addEventListener("click",calcular);
 function calcular(){
     const valor1 = parseInt(campo1.value);
     const valor2 = parseInt(campo2.value);
-   const operacao = seletor.value;
-  if(operacao==="Somar")
-  resposta.innerHTML = valor1 + valor2;
-  if(operacao==="Subtrair")
-  resposta.innerHTML = valor1 - valor2;
-  if(operacao==="Multiplicar")
-  resposta.innerHTML = valor1 * valor2;
-  if(operacao==="Dividir")
-  resposta.innerHTML = valor1 / valor2;
+    const operacao = seletor.value;
+    if(operacao==="Somar")
+        resposta.innerHTML = valor1 + valor2;
+    if(operacao==="Subtrair")
+        resposta.innerHTML = valor1 - valor2;
+    if(operacao==="Multiplicar")
+        resposta.innerHTML = valor1 * valor2;
+    if(operacao==="Dividir")
+        resposta.innerHTML = valor1 / valor2;
 }
